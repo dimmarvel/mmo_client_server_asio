4754e62fa3f7d95fd7b8235ce1d7371bbb4ff079
Dima Matsiukhov <dimmarvel@mail.ru>
2021-01-24 20:42:27 +0300

    Simple example of use

--- Networking/NetClient/SimpleClient.cpp
+++ Networking/NetClient/SimpleClient.cpp
@@ -7,28 +7,23 @@ enum class CustomMsgTypes : uint32_t
 	MovePlayer
 };
 
+class CustomClient : public olc::net::client_interface<CustomMsgTypes>
+{
+public:
+	bool FireBullet(float x, float y)
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::FireBullet;
+		msg << x << y;
+		Send(msg);
+	}
+};
+
 int main()
 {
-	olc::net::message<CustomMsgTypes> msg;
-	msg.header.id = CustomMsgTypes::FireBullet;
-
-	int a = 1;
-	bool b = true;
-	float c = 3.1415;
-
-	struct {
-		float x;
-		float y;
-	} d[5];
-
-	msg << a << b << c << d;
-
-	a = 99;
-	b = false;
-	c = 99.0f;
-
-	msg >> d >> c >> b >> a;
-
-
+	CustomClient c;
+	c.Connect("community.onelonecoder.com", 60000);
+	c.FireBullet(2.5f, 5.0f);
 	return 0;
+
 }
